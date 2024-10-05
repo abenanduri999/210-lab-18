98115be86f301c2a3a5d66e9864b7c95839c195d
Abe <abenanduri1@gmail.com>
2024-10-04 19:17:30 -0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // COMSC 210 | LAB 18 | Abhishek Nanduri 
 
 #include<iostream>
+#include<string>
 using namespace std; 
 
 struct Node
@@ -9,3 +10,48 @@ struct Node
     string review; 
     Node  * next; 
 };
+
+Node * addToHead(Node *); 
+Node * addToTail(Node *); 
+
+int main() {
+
+    Node * head = nullptr; 
+
+    int answer;
+    cout<<"Which linked list method should we use? (1 or 2)?"<<endl; 
+    cout<<"[1] New nodes are added at the head of the linked list"<<endl; 
+    cout<<"[2] New nodes are added at the tail of the linked list"<<endl;
+    cin>>answer; 
+
+    if (answer == 1)
+    {
+
+    }    
+    else if(answer == 2)
+    {
+
+    }
+    else 
+    {
+        cout<<"Invalid Input."<<endl; 
+    }
+
+
+}
+
+Node * addToHead ( Node * head)
+{
+    Node * newNode; 
+    double value; 
+    string rev; 
+    cout<<"Enter review rating (0 -5): "; 
+    cin>>value; 
+    cin.ignore(); 
+    cout<<"Enter a review: "; 
+    getline(cin, rev); 
+
+    if
+
+
+} 
